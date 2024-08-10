cb72488278cd0b412f2af367e982057e245c7e7a
fahad <fah8ad@outlook.com>
2024-08-10 18:26:34 +0300

    add

--- PaLM2/urls.py
+++ PaLM2/urls.py
@@ -4,13 +4,10 @@
 
 # Add a comment to clarify the import
 # Import the 'path' function from 'django.urls'
-# PaLM2/urls.py
-
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('generate-text/', views.text_form, name='text_form'),
+  path('generate-text/', views.text_form, name='text_form'),
     path('generate-text-only/', views.generate_text_only, name='generate_text_only'),
 ]
